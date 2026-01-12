bb32a80d26441f6758aa7be52d6f848d5372d60e
Shreya Porwal <porwalshreya11111@gmail.com>
2026-01-12 23:04:29 +0530

    code update for left right sum diff

--- solutions/Left_and_Right_Sum_differences.py
+++ solutions/Left_and_Right_Sum_differences.py
@@ -1,18 +1,39 @@
+# class Solution:
+#     def leftRightDifference(self, nums: List[int]) -> List[int]:
+#         tsum= 0
+#         for num in nums:
+#             tsum+=num
+#         l = len(nums)
+#         leftSum = []
+#         rightSum = []
+#         answer = []
+#         lsum =0
+#         for i in range(l):
+#             leftSum.append(lsum)
+#             lsum +=nums[i]
+#             rightSum.append(tsum-lsum)
+#             v = leftSum[i] - rightSum[i]
+#             if v>=0:
+#                 answer.append(v)
+#             else:
+#                 answer.append((-v))
+#         return answer
+
+
 class Solution:
     def leftRightDifference(self, nums: List[int]) -> List[int]:
         tsum= 0
         for num in nums:
             tsum+=num
         l = len(nums)
-        leftSum = []
-        rightSum = []
+
         answer = []
-        rsum =0
+        lsum =0
+        rsum=0
         for i in range(l):
-            leftSum.append(rsum)
-            rsum +=nums[i]
-            rightSum.append(tsum-rsum)
-            v = leftSum[i] - rightSum[i]
+            rsum =tsum-nums[i]-lsum
+            v = lsum - rsum
+            lsum+=nums[i]
             if v>=0:
                 answer.append(v)
             else:
